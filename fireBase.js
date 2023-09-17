a43c374096d88782ac1be70427d6283278b0da5a
CShumberto <144862788+CShumberto@users.noreply.github.com>
2023-09-16 22:51:18 -0300

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,16 @@
+
+// fireBase.js
+
+// Configuração do Firebase
+var firebaseConfig = {
+   apiKey: "AIzaSyC28CYNjHhUi8rOwrASXdRJ1cm2PV6fEW4",
+    authDomain: "controlemensal-sacolinhas.firebaseapp.com",
+    databaseURL: "https://controlemensal-sacolinhas-default-rtdb.firebaseio.com",
+    projectId: "controlemensal-sacolinhas",
+    storageBucket: "controlemensal-sacolinhas.appspot.com",
+    messagingSenderId: "925424065013",
+    appId: "1:925424065013:web:cf961586c6300ceb1468bc"
+};
+
+// Inicializar o Firebase com a configuração
+firebase.initializeApp(firebaseConfig);
