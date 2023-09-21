dc9d094d1f0751e72afe45a36c0d546bccfb86bf
CShumberto <144862788+CShumberto@users.noreply.github.com>
2023-09-20 23:31:46 -0300

    Update appsc.js

--- appsc.js
+++ appsc.js
@@ -1,46 +1,30 @@
-// Função para filtrar a tabela com base nos campos "Operador" e "Loja"
-function filtrarTabela() {
-    const filtroOperador = document.getElementById('filtroOperador').value.toLowerCase();
-    const filtroLoja = document.getElementById('filtroLoja').value.toLowerCase();
-    const userEmail = document.getElementById('userEmailDisplay').textContent;
-
-    const tbody = document.querySelector('tbody');
-    const linhas = tbody.getElementsByTagName('tr');
-
-    // Crie um objeto que mapeie os emails para os números das lojas
-    const emailToLoja = {
+document.addEventListener("DOMContentLoaded", function () {
+    // Crie um objeto que mapeie os emails para os valores das opções do dropdown
+    const emailToOptionValue = {
         'gestaofrentecaixa@gmail.com': 'Todas as Lojas',
         'fcaixa01@barbosasm.com.br': '1001',
         'fcaixa02@barbosasm.com.br': '1002',
-        // Adicione os demais emails e números das lojas aqui...
+        // Adicione os demais emails e valores das opções aqui...
     };
 
-    // Obtenha a loja correspondente ao email do usuário ou defina como "Todas as Lojas" por padrão
-    const lojaSelecionada = emailToLoja[userEmail] || 'Todas as Lojas';
+    // Obtenha o email do usuário logado
+    const userEmail = document.getElementById('userEmailDisplay').textContent;
 
-    // Itere pelas linhas da tabela e mostre/oculte com base nos filtros
-    for (let i = 0; i < linhas.length; i++) {
-        const linha = linhas[i];
-        const colunaOperador = linha.getElementsByTagName('td')[2];
-        const colunaLoja = linha.getElementsByTagName('td')[1];
-
-        if (colunaOperador && colunaLoja) {
-            const operador = colunaOperador.textContent.toLowerCase();
-            const loja = colunaLoja.textContent.toLowerCase();
-
-            // Verifique se a linha atende aos critérios de filtro
-            if (operador.includes(filtroOperador) && (loja === lojaSelecionada || lojaSelecionada === 'Todas as Lojas')) {
-                linha.style.display = '';
-            } else {
-                linha.style.display = 'none';
-            }
-        }
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
+    // Obtenha o elemento do dropdown
     const filtroLoja = document.getElementById('filtroLoja');
 
-    // Dispare o evento onchange para que a tabela seja filtrada automaticamente
-    filtroLoja.dispatchEvent(new Event('change'));
+    // Obtenha o valor da opção correspondente ao email do usuário
+    const optionValue = emailToOptionValue[userEmail];
+
+    // Se a opção correspondente for encontrada, selecione-a no dropdown e acione o evento de clique
+    if (optionValue) {
+        // Selecione a opção correspondente
+        filtroLoja.value = optionValue;
+
+        // Crie um evento de clique
+        const eventoClick = new Event('click');
+
+        // Acione o evento de clique no dropdown
+        filtroLoja.dispatchEvent(eventoClick);
+    }
 });
