94a8105715a506b8a33a23f0490884644ad969f3
CShumberto <144862788+CShumberto@users.noreply.github.com>
2023-09-21 20:03:30 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+ // Configuração do Firebase
+        const firebaseConfig = {
+           apiKey: "AIzaSyC28CYNjHhUi8rOwrASXdRJ1cm2PV6fEW4",
+    authDomain: "controlemensal-sacolinhas.firebaseapp.com",
+    databaseURL: "https://controlemensal-sacolinhas-default-rtdb.firebaseio.com",
+    projectId: "controlemensal-sacolinhas",
+    storageBucket: "controlemensal-sacolinhas.appspot.com",
+    messagingSenderId: "925424065013",
+    appId: "1:925424065013:web:cf961586c6300ceb1468bc"
+        };
+
+// Inicialize o Firebase
+        firebase.initializeApp(firebaseConfig);
