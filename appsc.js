e1cb57ae7bb0250ad1e4efb3c89a4b7831d8c802
CShumberto <144862788+CShumberto@users.noreply.github.com>
2023-09-20 23:48:39 -0300

    Update appsc.js

--- appsc.js
+++ appsc.js
@@ -1,30 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Crie um objeto que mapeie os emails para os valores das opções do dropdown
-    const emailToOptionValue = {
-        'gestaofrentecaixa@gmail.com': 'Todas as Lojas',
+    // Crie um objeto que mapeie os emails para as lojas correspondentes
+    const emailToLoja = {
+        'gestaofrentecaixa@gmail.com': null, // null representa todas as lojas
         'fcaixa01@barbosasm.com.br': '1001',
         'fcaixa02@barbosasm.com.br': '1002',
-        // Adicione os demais emails e valores das opções aqui...
+        // Adicione os demais emails e lojas aqui...
     };
 
     // Obtenha o email do usuário logado
     const userEmail = document.getElementById('userEmailDisplay').textContent;
 
-    // Obtenha o elemento do dropdown
-    const filtroLoja = document.getElementById('filtroLoja');
+    // Obtenha a tabela DataTable (substitua 'tabela' pelo seletor correto)
+const data = $("#tabela-dados").DataTable();
 
-    // Obtenha o valor da opção correspondente ao email do usuário
-    const optionValue = emailToOptionValue[userEmail];
-
-    // Se a opção correspondente for encontrada, selecione-a no dropdown e acione o evento de clique
-    if (optionValue) {
-        // Selecione a opção correspondente
-        filtroLoja.value = optionValue;
-
-        // Crie um evento de clique
-        const eventoClick = new Event('click');
-
-        // Acione o evento de clique no dropdown
-        filtroLoja.dispatchEvent(eventoClick);
-    }
+    // Defina a função de filtro com base no email do usuário
+    tabela.column(2).search(emailToLoja[userEmail] || '', true, false).draw();
 });
